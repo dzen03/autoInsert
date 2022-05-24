667fc555d8f71b02ff8ad7f000e25d05a50c4d9e
dzen03 <43926347+dzen03@users.noreply.github.com>
2022-05-24 19:12:33 +0300

    added print of datablocks

--- insert.py
+++ insert.py
@@ -12,9 +12,36 @@ def hex_to_bin(a):
     return bin(int(a, 16))[2:].zfill(16)
 
 
+def bin_to_hex(a):
+    return hex(int(a, 2))[2:].zfill(3).upper()
+
+
 program = []
 startReg = hex_to_bin('0')
 fileHash = ''
+
+
+def sum_hex(a, b):
+    return hex((int(a, 16) + int(b, 16)) + 1)[2:]
+
+
+def data_type(comm, data):
+    tmp = hex_to_bin(data)[::-1]
+    # return tmp
+    if tmp[11] == '0':
+        return f'${bin_to_hex(tmp[10::-1])}'
+    if tmp[8:12] == '1111':
+        return f'#{bin_to_hex(tmp[7::-1])}'
+    if tmp[8:12] == '0111':
+        return f'(IP + {bin_to_hex(tmp[7::-1]).upper().zfill(3)})', f'({sum_hex(comm, bin_to_hex(tmp[7::-1])).upper().zfill(3)})'
+    if tmp[8:12] == '0011':
+        return f'(SP + {bin_to_hex(tmp[7::-1])})'
+    if tmp[8:12] == '1101':
+        return f'-({bin_to_hex(tmp[7::-1])})'
+    if tmp[8:12] == '0101':
+        return f'({bin_to_hex(tmp[7::-1])})+'
+    if tmp[8:12] == '0001':
+        return f'({bin_to_hex(tmp[7::-1])})'
 
 
 def pre_main():
@@ -34,7 +61,7 @@ def pre_main():
         if start == '+':
             startReg = reg
         program.append((reg, data))
-        print(reg, data)
+        print(reg, data, data_type(reg, data))
 
 
 def main():
@@ -51,8 +78,11 @@ def main():
 
 
 if __name__ == '__main__':
-    pre_main()
-    while True:
-        keyboard.wait(HOTKEY)
-        if 'БЭВМ' in pyautogui.getActiveWindowTitle():
-            main()
+    try:
+        pre_main()
+        while True:
+            keyboard.wait(HOTKEY)
+            if 'БЭВМ' in pyautogui.getActiveWindowTitle():
+                main()
+    except KeyboardInterrupt:
+        exit(0)
